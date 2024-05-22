202126e3a81a283df6b23070759659dc1c255c79
SreenandPP <sreenandpp@gmail.com>
2024-05-22 12:13:20 +0530

    first commit

---
+++ Middlewares/jwtMiddleware.js
@@ -0,0 +1,28 @@
+const jwt=require('jsonwebtoken')
+
+
+const jwtMiddlewareFun=(req,res,next)=>{
+    console.log("insidejwtMiddleware");
+    // res.status(404).json("middle")
+    try{
+        const token=req.headers.authorization.split(" ")[1]
+        console.log(token);
+        if(token){
+            const result=jwt.verify(token,process.env.secret_key)
+            console.log(result); 
+            req.payload=result.userId
+            next()
+        }
+        else{
+            res.status(406).json("Please Login First!!")
+        }
+     
+    }
+    catch(err){
+        console.log(err);
+        res.status(406).json("Please Login ")
+    }
+    
+}
+
+module.exports=jwtMiddlewareFun
---
+++ Middlewares/multerMiddleware.js
@@ -0,0 +1,27 @@
+const multer=require('multer')
+
+const storage=multer.diskStorage({
+    destination:(req,file,callback)=>{
+      callback(null,'./uploads')
+    },
+    filename:(req,file,callback)=>{
+       callback(null,`Img-${Date.now()}-${file.originalname}`)
+    }
+})
+
+
+const filefilter=(req,file,callback)=>{
+    if(file.mimetype=="image/jpg" || file.mimetype=="image/jpeg" || file.mimetype=="image/png" ){
+        callback(null,true)
+    }
+    else{
+        callback(null,false)
+        return callback(new Error("Please uploadnfile with following extensions (jpg,jpeg,png ) only!"))
+    }
+}
+
+const multerconfig=multer({
+    storage,filefilter
+})
+
+module.exports=multerconfig
